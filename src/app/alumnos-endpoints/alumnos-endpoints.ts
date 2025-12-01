34e7d6d18b4bfc95ce7b88808e97fb7d19db0157
Cesar Ochoa <cesarin117@outlook.com>
2025-12-01 15:52:53 -0600

    Se agregó adminLTE y un menu lateral... aún faltan algunos cambios ... pero es avance grafico

---
+++ src/app/alumnos-endpoints/alumnos-endpoints.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-endpoints',
+  imports: [],
+  templateUrl: './alumnos-endpoints.html',
+  styleUrl: './alumnos-endpoints.css',
+})
+export class AlumnosEndpoints {
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EscuelasEndpoints } from './escuelas-endpoints/escuelas-endpoints';
+import { AlumnosEndpoints } from './alumnos-endpoints/alumnos-endpoints';
 
 //Estos son decoradores
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,EscuelasEndpoints],
+  imports: [RouterOutlet,EscuelasEndpoints,AlumnosEndpoints],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 //Esta es la definicion de nuestra clase, la cual se llama App
 export class App {
-  protected readonly title = "juan es jot mundo xd";
+  protected readonly title = "juan ES JOTO";
   protected readonly variableRandom = "Cosas para escribir xd";
+  valor = 0;
+  contador() {
+    return this.valor = this.valor + 1;
+  }
 }
